842a398aff989c2ce860e32498b3db8706b761f7
Niranjan Kurambhatti <niranjan.kurambhatti@gmail.com>
2025-10-24 01:28:51 -0500

    Revert "Delete test.md file"
    This reverts commit ced4fed731026f84475757d70a72f391dadc3009.

--- backend/encore.gen/clients/index.js
+++
@@ -1,3 +0,0 @@
-export * as frontend from "../internal/clients/frontend/endpoints.js";
-export * as run from "../internal/clients/run/endpoints.js";
-export * as steering from "../internal/clients/steering/endpoints.js";
