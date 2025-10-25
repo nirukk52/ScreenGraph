77b2a1135d2e19c1a737f2ab9da2650a790346ff
Niranjan Kurambhatti <niranjan.kurambhatti@gmail.com>
2025-10-25 04:38:51 -0500

    fix(deploy): include encore.gen in repo for cloud builds

---
+++ encore.gen/clients/index.js
@@ -0,0 +1,3 @@
+export * as run from "../internal/clients/run/endpoints.js";
+export * as steering from "../internal/clients/steering/endpoints.js";
+export * as frontend from "../internal/clients/frontend/endpoints.js";
